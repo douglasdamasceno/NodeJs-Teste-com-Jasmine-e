cc096bed2fa07e87710cd5333c9677f351a1691d
Douglas Damasceno <douglasmdamasceno@gmail.com>
2022-03-26 15:26:13 -0300

    configurações de teste com Jasmine e karma

---
+++ spec/exemplo/exempleSpec.js
@@ -0,0 +1,8 @@
+describe("Suite de teste de exemplo com NodeJs", function () {
+    it("Deve demonstrar o uso do Jasmine com NodeJs", function () {
+        expect(true).toEqual(true);
+    })
+    it("Deve 2 demonstrar o uso do Jasmine com NodeJs", function () {
+        expect(false).toEqual(false);
+    })
+})
